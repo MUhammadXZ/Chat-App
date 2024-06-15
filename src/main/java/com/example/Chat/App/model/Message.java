0430fc9ca0babcfd41ccdbb019b243c5f8c97add
MUhammadXZ <muhammadfakhr73@gmail.com>
2024-06-16 01:18:27 +0300

    model: Message

---
+++ src/main/java/com/example/Chat/App/model/Message.java
@@ -0,0 +1,57 @@
+package com.example.Chat.App.model;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "messages")
+public class Message {
+    @Id
+    private String id;
+    private String sender;
+    private String content;
+    private LocalDateTime timestamp;
+
+    public Message() {
+        this.id = id;
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+// Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
